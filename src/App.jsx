e2a33f367e2696c6e6bd2dc42a5e5307a29e9bd6
Alexandra Rhodes <atperry30@gmail.com>
2023-03-20 21:29:34 -0700

    Moved state variable and added sidebar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
-import './App.css'
-import Card from './components/Card'
+import { useState } from "react";
+import "./App.css";
+import Card from "./components/Card";
+import HistorySidebar from "./components/HistorySidebar";
+import BanSidebar from "./components/BanSidebar";
 
 function App() {
+  const [visitedCats, setVisitedCats] = useState([]);
+  const [bannedOrigins, setBannedOrigins] = useState([]);
 
   return (
     <div className="App">
-      <Card />
+      <HistorySidebar visitedCats={visitedCats} />
+
+      <Card
+        visitedCats={visitedCats}
+        setVisitedCats={setVisitedCats}
+        bannedOrigins={bannedOrigins}
+        setBannedOrigins={setBannedOrigins}
+      />
+
+      <BanSidebar bannedOrigins={bannedOrigins} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
